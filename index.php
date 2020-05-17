38aae7aa3e6f9fbb6d66328965fe89b6dd571115
artem <neznajkinartem357@gmail.com>
2020-05-17 10:37:55 +0600

    First

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-use HTML\Tag;
-require_once "autoload.php";
+require_once 'Matrix.php';
 
-echo Tag::div();
+$matA = new Matrix([[1,3,4], [2,4,6], [4,3,7]]);
+$matB = new Matrix([[4,3,7], [2,4,6], [1,3,4]]);
+$matA->add($matB);
+print_r($matA);
+$matA->diff($matB);
+print_r($matA);
+$matA->mult($matB);
+print_r($matA);
