413ec0940485bd54a0b136204e53ab1b6754d201
artem <neznajkinartem357@gmail.com>
2020-05-17 10:58:23 +0600

    First

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
-require_once 'Matrix.php';
+require_once 'app/HTML/Doctype.php';
+use HTML\Doctype;
 
-$matA = new Matrix([[1,3,4], [2,4,6], [4,3,7]]);
-$matB = new Matrix([[4,3,7], [2,4,6], [1,3,4]]);
-$matA->add($matB);
-print_r($matA);
-$matA->diff($matB);
-print_r($matA);
-$matA->mult($matB);
-print_r($matA);
+$doctype = new Doctype();
+echo $doctype->get('XHTML 1.1');
