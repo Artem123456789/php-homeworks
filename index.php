109141e68f0a36e284192a160c1279b87228abb3
artem <neznajkinartem357@gmail.com>
2020-05-19 11:44:15 +0600

    First

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
-require_once 'app/HTML/Doctype.php';
-use HTML\Doctype;
+require_once 'Hello.php';
 
-$doctype = new Doctype();
-echo $doctype->get('XHTML 1.1');
+$requestUri = $_SERVER['REQUEST_URI'];
+$requestUri = str_replace([',', '!', ',', '.', '?', ';', '-', '+', '*',
+    '=', '&', '^', '$', '%', '#', '@', '(', ')', '<', '>', '"'], '', $requestUri);
+$requestUri = explode('/', $requestUri);
+
+if(class_exists($requestUri[1])){
+    $hello = new Hello();
+    if(method_exists($hello, $requestUri[2])){
+        echo $hello->world();
+    }
+}
